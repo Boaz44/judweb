14374d4a6ad1e55792ee1f164f83e0a37afd2f34
Emmanuel <you@yourdomain.example.com>
2016-07-18 12:37:45 +0200

    added the home page with nav menu

--- judweb/app/Http/routes.php
+++ judweb/app/Http/routes.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hom');
+});
+Route::get('ihome/', function () {
+    return view('ihome');
 });
---
+++ judweb/resources/views/hom.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('css/homestyle.css') }}">
+<title>Home</title>
+<body>
+    <header>
+	<img src="../images/6.png" />
+	<nav>
+	<ul id="nav">
+	
+		<li><a href="#">HOME</a></li>
+		<li>
+			<a href="#">ABOUT US</a>
+			<ul>
+				<li><a href="#">MISSION STATEMENT</a></li>
+				<li><a href="#">VISION</a></li>
+				<li><a href="#">GOAL</a></li>
+				<li><a href="#">VISION</a></li>
+				<li><a href="#">CORE VALUES</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">COURTS</a>
+			<ul>
+				<li><a href="#">SUPREME COURTS</a></li>
+				<li><a href="#">HIGH COURTS</a></li>
+				<li><a href="#">MAGISTRATE COURTS</a></li>
+			</ul>
+		</li>
+		<li><a href="#">COURT PROCESSES</a>
+			<ul>
+				<li><a href="#">SUPREME COURTS PROCESSES</a></li>
+				<li><a href="#">HIGH COURTS PROCESSES</a></li>
+				<li><a href="#">MAGISTRATE COURTS PROCESSES</a></li>
+				<li><a href="#">ADDRESSING JUDICIAL OFFICERS</a></li>
+			</ul>
+		</li>
+		<li><a href="#">CAUSE LIST</a></li>
+		<li><a href="#">JUDGEMENTS</a></li>
+		<li>
+		<a href="#">PUBLICATIONS</a>
+		<ul>
+				<li><a href="#">NEWS</a></li>
+				<li><a href="#">REPORTS</a></li>
+				<li><a href="#">ANNOUNCEMENTS</a></li>
+				<li><a href="#">RESEARCH</a></li>
+			</ul>
+		</li>
+		<li><a href="#">STAFF</a></li>
+		<li><a href="#">GALLERY</a></li>
+	</ul>
+	</nav>
+	</header>
+	<div class="main-content">
+		
+	</div>
+</body>
+</html>
